7d883d8f41a1ebfe69de64ae5bf2e38c0a6fe4be
unknown <athina.papageorgioukoufidou@epfl.ch>
2023-03-07 14:52:14 +0100

    Feat: get location of user and center map on it [incomplete]

--- app/src/main/java/com/github/studydistractor/sdp/MapsActivity.kt
+++ app/src/main/java/com/github/studydistractor/sdp/MapsActivity.kt
@@ -4,43 +4,169 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.github.studydistractor.sdp.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
+import com.google.android.libraries.places.api.net.PlacesClient
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var placesClient: PlacesClient
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var locationPermissionGranted = false
+    private var map: GoogleMap? = null
+    private var lastKnownLocation: Location? = null
+// TODO: put sat as default location
+    private val defaultLocation = LatLng(-33.8523341, 151.2106085)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMapsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_maps);
+        Places.initialize(applicationContext, BuildConfig.MAPS_API_KEY);
+        placesClient = Places.createClient(this);
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+//        binding = ActivityMapsBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+//        val mapFragment = supportFragmentManager
+//            .findFragmentById(R.id.map) as SupportMapFragment
+//        mapFragment.getMapAsync(this)
+
         val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+            .findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+        this.map = googleMap
+        updateLocationUI()
+        getDeviceLocation()
+//        mMap = googleMap
+//
+//        val sat = LatLng(46.520544, 6.567825)
+//        mMap.addMarker(MarkerOptions().position(sat).title("Satellite"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sat))
+//        mMap.animateCamera(CameraUpdateFactory.zoomTo(17f))
+//
+//        mMap.setOnInfoWindowClickListener {
+//            val lat = it.position.latitude
+//            val lng = it.position.longitude
+//            val toast = Toast.makeText(applicationContext, "Latitude: $lat, Longitude: $lng", Toast.LENGTH_SHORT)
+//            toast.show()
+//        }
+    }
 
-        val sat = LatLng(46.520544, 6.567825)
-        mMap.addMarker(MarkerOptions().position(sat).title("Satellite"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sat))
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(17f))
-
-        mMap.setOnInfoWindowClickListener {
-            val lat = it.position.latitude
-            val lng = it.position.longitude
-            val toast = Toast.makeText(applicationContext, "Latitude: $lat, Longitude: $lng", Toast.LENGTH_SHORT)
-            toast.show()
+    private fun getLocationPermission() {
+        /*
+         * Request location permission, so that we can get the location of the
+         * device. The result of the permission request is handled by a callback,
+         * onRequestPermissionsResult.
+         */
+        if (ContextCompat.checkSelfPermission(this.applicationContext,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true
+        } else {
+//            TODO: create constant for request code
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                1)
         }
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        locationPermissionGranted = false
+        when (requestCode) {
+            1 -> {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true
+                }
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+        updateLocationUI()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun updateLocationUI() {
+        if (map == null) {
+            return
+        }
+        try {
+            if (locationPermissionGranted) {
+                map?.isMyLocationEnabled = true
+                map?.uiSettings?.isMyLocationButtonEnabled = true
+            } else {
+                map?.isMyLocationEnabled = false
+                map?.uiSettings?.isMyLocationButtonEnabled = false
+                lastKnownLocation = null
+                getLocationPermission()
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception:", e.message, e)
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getDeviceLocation() {
+        /*
+         * Get the best and most recent location of the device, which may be null in rare
+         * cases when a location is not available.
+         */
+        try {
+            if (locationPermissionGranted) {
+                val locationResult = fusedLocationProviderClient.lastLocation
+                locationResult.addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Set the map's camera position to the current location of the device.
+                        lastKnownLocation = task.result
+                        if (lastKnownLocation != null) {
+                            map?.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                LatLng(lastKnownLocation!!.latitude,
+                                    lastKnownLocation!!.longitude), 15f))
+                        }
+                    } else {
+                        Log.d("TAG", "Current location is null. Using defaults.")
+                        Log.e("TAG", "Exception: %s", task.exception)
+                        map?.moveCamera(CameraUpdateFactory
+                            .newLatLngZoom(defaultLocation, 15f))
+                        map?.uiSettings?.isMyLocationButtonEnabled = false
+                    }
+                }
+            }
+        } catch (e: SecurityException) {
+            Log.e("Exception: %s", e.message, e)
+        }
+    }
+
+//    TODO: get location of user and center map on it
+//    TODO: add marker on user location
+//    TODO: add markers for interesting locations
+//    TODO: clicking on marker takes user to different activity
+
 }
