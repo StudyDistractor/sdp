2a8c222b1309b20a57054fb8e8a7f62ad5de95bc
bluedrack <bluedrack28@gmail.com>
2023-03-15 15:54:42 +0100

    Fixing Android manifest and gradle

--- app/src/androidTest/java/com/github/studydistractor/sdp/RegisterActivityTest.kt
+++ app/src/androidTest/java/com/github/studydistractor/sdp/RegisterActivityTest.kt
@@ -1,11 +1,9 @@
 package com.github.studydistractor.sdp
 import android.content.Context
-import android.util.Log
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.matcher.ViewMatchers.assertThat
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
@@ -13,7 +11,6 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import kotlinx.coroutines.tasks.await
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
