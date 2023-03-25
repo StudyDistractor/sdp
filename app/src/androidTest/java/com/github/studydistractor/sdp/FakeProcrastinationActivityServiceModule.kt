810423e035dce0b3a2eeac3a2296a9cee9105a31
qngo <quang.ngo@epfl.ch>
2023-03-25 17:55:16 +0100

    Everything is implented, just need to fix the JaCoCo bug for test coverage

---
+++ app/src/androidTest/java/com/github/studydistractor/sdp/FakeProcrastinationActivityServiceModule.kt
@@ -0,0 +1,36 @@
+package com.github.studydistractor.sdp
+
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import javax.inject.Singleton
+
+@Module
+@TestInstallIn(
+    components = [SingletonComponent::class],
+    replaces = [ProcrastinationActivityServiceModule::class]
+)
+object FakeProcrastinationActivityServiceModule {
+
+    @Singleton
+    @Provides
+    fun provideFakeService() : ProcrastinationActivityService {
+        return FakeProcrastinationActivityService()
+    }
+}
+
+class FakeProcrastinationActivityService : ProcrastinationActivityService{
+
+    private val activityList = SnapshotStateList<ProcrastinationActivity>()
+
+    override fun fetchProcrastinationActivities(): SnapshotStateList<ProcrastinationActivity> {
+        return this.activityList
+    }
+
+    override fun postProcastinationActivities(activity: ProcrastinationActivity) {
+        activityList.add(activity)
+    }
+}
--- app/src/main/java/com/github/studydistractor/sdp/ProcrastinationActivityServiceModule.kt
+++ app/src/main/java/com/github/studydistractor/sdp/ProcrastinationActivityServiceModule.kt
@@ -4,11 +4,14 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(SingletonComponent::class)
 object ProcrastinationActivityServiceModule {
 
+    @Singleton
     @Provides
     fun provideProcrastinationAcitvityService() : ProcrastinationActivityService {
         return FireBaseProcrastinationActivityService()
