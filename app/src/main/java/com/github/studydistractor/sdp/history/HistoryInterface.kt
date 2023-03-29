f071e335292e69a44e3e072c99078844b18a69e5
bluedrack <bluedrack28@gmail.com>
2023-03-29 17:04:00 +0200

    Fix: fixing AndroidManifest to work and unnecessary import from HistoryInterface

--- app/src/main/java/com/github/studydistractor/sdp/history/HistoryInterface.kt
+++ app/src/main/java/com/github/studydistractor/sdp/history/HistoryInterface.kt
@@ -1,9 +1,6 @@
 package com.github.studydistractor.sdp.history
 
 import androidx.compose.runtime.snapshots.SnapshotStateList
-import com.github.studydistractor.sdp.ProcrastinationActivity
-import com.google.android.gms.tasks.Task
-import com.google.firebase.database.DataSnapshot
 
 interface HistoryInterface {
     /**
