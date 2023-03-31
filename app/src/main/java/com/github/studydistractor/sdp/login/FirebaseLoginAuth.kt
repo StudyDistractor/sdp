003d6ab0f63e00ecc4e94c67289ed3c25e57003c
Athena <athenapapakouf@gmail.com>
2023-03-31 04:42:59 +0200

    Fix: merge conflict

--- app/src/main/java/com/github/studydistractor/sdp/login/FirebaseLoginAuth.kt
+++ app/src/main/java/com/github/studydistractor/sdp/login/FirebaseLoginAuth.kt
@@ -9,16 +9,12 @@ import javax.inject.Inject
     This is a dummy implementation of the interface to show how Hilt works.
  */
 class FirebaseLoginAuth @Inject constructor() : LoginAuthInterface {
-<<<<<<< HEAD
     lateinit var auth : FirebaseAuth
 
     constructor(auth: FirebaseAuth) : this() {
         this.auth = auth
     }
 
-=======
-    var auth : FirebaseAuth = FirebaseAuth.getInstance()
->>>>>>> 8352636b1ec7fe6cebf193e3266ec2621f43c473
     override fun loginWithEmail(email: String, password: String): Task<AuthResult> {
         return auth.signInWithEmailAndPassword(email, password)
     }
