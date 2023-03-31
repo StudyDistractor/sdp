ceab5d7442d2cc1bdeb2c44bc18aaa01261ea4cd
Athena <athenapapakouf@gmail.com>
2023-03-31 04:23:06 +0200

    Fix: ProcrastinationActivityService uses updated fetchProcrastinationActivities

--- app/src/main/java/com/github/studydistractor/sdp/procrastinationActivity/ProcrastinationActivityService.kt
+++ app/src/main/java/com/github/studydistractor/sdp/procrastinationActivity/ProcrastinationActivityService.kt
@@ -3,13 +3,13 @@ package com.github.studydistractor.sdp.procrastinationActivity
 import androidx.compose.runtime.snapshots.SnapshotStateList
 
 interface ProcrastinationActivityService {
-    fun fetchProcrastinationActivities(callback: (List<ProcrastinationActivity>) -> Unit)
 
     /**
-     * Fetch all the procrastination activities available in the database
-     * @return a snapshotStateList that contains the procrastination activities
+     * Fetch the activities from the database
+     * @param callback the callback to be called when the activities are fetched
      */
-    fun fetchProcrastinationActivities() : SnapshotStateList<ProcrastinationActivity>
+    fun fetchProcrastinationActivities(callback: (List<ProcrastinationActivity>) -> Unit)
+
 
     /**
      * Post the activity to the database
