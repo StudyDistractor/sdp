f2cec56a359d9cb799374a6bee0d40221799db1b
Athena <athenapapakouf@gmail.com>
2023-04-16 22:43:04 +0200

    Feat: add button to complete activity

--- app/src/main/java/com/github/studydistractor/sdp/ui/DistractionScreen.kt
+++ app/src/main/java/com/github/studydistractor/sdp/ui/DistractionScreen.kt
@@ -1,17 +1,23 @@
 package com.github.studydistractor.sdp.ui
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.github.studydistractor.sdp.distraction.DistractionViewModel
 
 @Composable
-fun DistractionScreen(distractionViewModel : DistractionViewModel) {
+fun DistractionScreen(distractionViewModel: DistractionViewModel) {
+    val context = LocalContext.current
     Column(
         modifier = Modifier.fillMaxWidth().fillMaxHeight().padding(26.dp),
         verticalArrangement = Arrangement.Center,
@@ -25,7 +31,21 @@ fun DistractionScreen(distractionViewModel : DistractionViewModel) {
         Spacer(modifier = Modifier.height(4.dp))
         Text(
             text = distractionViewModel.distraction!!.description!!,
-            modifier = Modifier.testTag("description")
+            fontSize = 24.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .testTag("description")
+                .padding(8.dp)
         )
+        Button(onClick = {
+            Toast.makeText(
+                context,
+                "Activity completed!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }, modifier = Modifier.testTag("completeButton")) {
+            Text(text = "Activity completed!", color = Color.White)
+
+        }
     }
 }
