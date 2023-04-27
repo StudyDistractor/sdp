dc7a8e43ac4e2b615de818fdc26a8ffee2d2e1e9
lipefree <quang.ngo@epfl.ch>
2023-04-27 17:29:39 +0200

    feat: allows distraction list to be updated more than once

--- app/src/main/java/com/github/studydistractor/sdp/distraction/FireBaseDistractionService.kt
+++ app/src/main/java/com/github/studydistractor/sdp/distraction/FireBaseDistractionService.kt
@@ -3,6 +3,7 @@ package com.github.studydistractor.sdp.distraction
 import android.util.Log
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
@@ -19,12 +20,14 @@ class FireBaseDistractionService @Inject constructor(): DistractionService {
     val distractions =  mutableStateListOf<Distraction>()
 
     init {
-        databaseRef.addListenerForSingleValueEvent(object : ValueEventListener {
+        databaseRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                for(activity in snapshot.children) {
-                    val activityItem = activity.getValue(Distraction::class.java)
-                    if(activityItem != null) {
-                        distractions.add(activityItem)
+                distractions.clear()
+                for(distraction in snapshot.children) {
+                    val distractionItem = distraction.getValue(Distraction::class.java)
+                    val key = distraction.key
+                    if(distractionItem != null) {
+                        distractions.add(distractionItem)
                     }
                 }
             }
