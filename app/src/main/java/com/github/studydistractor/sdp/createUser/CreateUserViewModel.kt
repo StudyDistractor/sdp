2aadae674d5d3da1b4e42ff27ac50e3ce885187e
Adrien Bouquet <adrien.bouquet@epfl.ch>
2023-05-04 21:35:37 +0200

    fix(#112): Map to TimeStamp done (see the PR #123)

--- app/src/main/java/com/github/studydistractor/sdp/createUser/CreateUserViewModel.kt
+++ app/src/main/java/com/github/studydistractor/sdp/createUser/CreateUserViewModel.kt
@@ -1,13 +1,19 @@
 package com.github.studydistractor.sdp.createUser
 
+import android.os.Build
+import android.telephony.PhoneNumberUtils
+import androidx.annotation.RequiresApi
 import androidx.lifecycle.ViewModel
 import com.github.studydistractor.sdp.data.UserData
 import com.github.studydistractor.sdp.ui.state.CreateUserUiState
 import com.google.android.gms.tasks.Task
+import com.google.android.gms.tasks.Tasks
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import java.text.ParseException
+import java.text.SimpleDateFormat
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.util.Calendar
@@ -15,6 +21,7 @@ import java.util.Calendar
 class CreateUserViewModel(
     createUserModel: CreateUserModel
 ) : ViewModel() {
+    private val MAX_NAME_LENGTH = 255
     private val _createUserModel: CreateUserModel = createUserModel
     private val _uiState = MutableStateFlow(CreateUserUiState())
     private val _calendar = Calendar.getInstance()
@@ -28,12 +35,45 @@ class CreateUserViewModel(
         setDate(year, month, dayOfMonth)
     }
 
+    /**
+     * Check if the given phone number is a global phone number (using PhoneNumberUtils library)
+     * @param phoneNumber The phone number to check
+     * @throws IllegalArgumentException if the given phone number is null or invalid
+     */
+    private fun checkPhoneFormat(phoneNumber: String): Boolean {
+        return !(phoneNumber.isEmpty() || !PhoneNumberUtils.isGlobalPhoneNumber(phoneNumber))
+    }
+
+    /**
+     * Check if the given name is not empty and it does not exceed or is equal to 255 characters
+     * @param name The name to check
+     * @throws IllegalArgumentException if the name is not correctly formatted
+     */
+    private fun checkNameFormat(name: String): Boolean{
+        return !(name.isEmpty() || name.length >= MAX_NAME_LENGTH)
+    }
+
+    private fun checkDateFormat(date: String): Boolean {
+        try {
+            SimpleDateFormat("yyyy-mm-dd").parse(uiState.value.dateText)
+        } catch (pe: ParseException) {
+            return false
+        }
+        return true
+    }
+
     private fun refreshDateText() {
-        val dateText = LocalDate.of(
-            uiState.value.year,
-            uiState.value.month,
-            uiState.value.dayOfMonth
-        ).format(DateTimeFormatter.ISO_LOCAL_DATE)
+        val y =  uiState.value.year
+        val m =  uiState.value.month
+        val d =  uiState.value.dayOfMonth
+
+        val dateText = "$y-$m-$d"
+
+        //val dateText = LocalDate.of(
+        //            uiState.value.year,
+        //            uiState.value.month,
+        //            uiState.value.dayOfMonth
+        //        ).format(DateTimeFormatter.ISO_LOCAL_DATE)
 
         _uiState.update {
             it.copy(
@@ -46,7 +86,7 @@ class CreateUserViewModel(
         _uiState.update {
             it.copy(
                 year = year,
-                month = month,
+                month = month + 1,
                 dayOfMonth = dayOfMonth
             )
         }
@@ -70,16 +110,31 @@ class CreateUserViewModel(
     }
 
     fun createUser(): Task<Void> {
-        val birthday = hashMapOf(
-            "year" to uiState.value.year,
-            "month" to uiState.value.month,
-            "day" to uiState.value.dayOfMonth
-        )
+
+        if (!checkNameFormat(uiState.value.firstname)) {
+            return Tasks.forException(Exception("firstname is invalid"))
+        }
+
+        if (!checkNameFormat(uiState.value.lastname)) {
+            return Tasks.forException(Exception("lastname is invalid"))
+        }
+
+        if (!checkPhoneFormat(uiState.value.phoneNumber)) {
+            return Tasks.forException(Exception("phoneNumber is invalid"))
+        }
+
+        val timestamp = if (checkDateFormat(uiState.value.dateText)){
+            (SimpleDateFormat("yyyy-mm-dd").parse(uiState.value.dateText).time/1000L).toInt()
+        } else {
+            return Tasks.forException(Exception("Invalid Date format"))
+        }
+
+
         return _createUserModel.createUser(UserData(
             firstname = uiState.value.firstname,
             lastname = uiState.value.lastname,
             phone = uiState.value.phoneNumber,
-            birthday = birthday,
+            birthday = timestamp,
             score = 0
         ))
     }
