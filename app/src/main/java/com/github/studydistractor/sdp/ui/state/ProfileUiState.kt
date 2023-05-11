3f6f7f9afb5fa418747b7605f885bbefc9802412
Adrien Bouquet <adrien.bouquet@epfl.ch>
2023-05-11 20:40:09 +0200

    feat(#110): Profile screen

---
+++ app/src/main/java/com/github/studydistractor/sdp/ui/state/ProfileUiState.kt
@@ -0,0 +1,8 @@
+package com.github.studydistractor.sdp.ui.state
+
+data class ProfileUiState(
+    val fullname: String = "",
+    val birthday: String = "",
+    val phone: String = "",
+    val score: Int = 0,
+)
