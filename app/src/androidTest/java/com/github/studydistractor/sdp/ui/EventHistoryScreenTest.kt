70f6173298dd8fc52e2ae37f17f71208ec8076e7
abouquet27 <adrien.bouquet@epfl.ch>
2023-05-18 14:09:37 +0200

    fix: ui

--- app/src/androidTest/java/com/github/studydistractor/sdp/ui/EventHistoryScreenTest.kt
+++ app/src/androidTest/java/com/github/studydistractor/sdp/ui/EventHistoryScreenTest.kt
@@ -1,11 +1,14 @@
 package com.github.studydistractor.sdp.ui
 
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.assert
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertTextContains
+import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
 import com.github.studydistractor.sdp.createUser.CreateUserViewModel
 import com.github.studydistractor.sdp.eventChat.EventChatViewModel
 import com.github.studydistractor.sdp.eventHistory.EventHistoryViewModel
@@ -33,6 +36,7 @@ class EventHistoryScreenTest {
     @Test
     fun testTitleIsDisplayed(){
         composeRule.onNodeWithTag("event-history-screen__title").assertIsDisplayed()
+        composeRule.onNodeWithTag("event-history-screen__title").assertTextContains("Your event history")
     }
 
     @Test
@@ -54,6 +58,51 @@ class EventHistoryScreenTest {
     fun testCardsThatShouldNotBeDisplayed(){
         composeRule.onNodeWithTag("event-history-card__title NotFinishedEvent").assertDoesNotExist()
         composeRule.onNodeWithTag("event-history-card__title UserHasNotTakenPartIn").assertDoesNotExist()
+    }
+
+    @Test
+    fun testChatButtonIsDisplayed(){
+        composeRule.onNodeWithTag("event-history-card__chat-button event1").assertExists()
+        composeRule.onNodeWithTag("event-history-card__chat-button event1").assertIsDisplayed()
+        composeRule.onNodeWithTag("event-history-card__chat-button event1").assertHasClickAction()
+        composeRule.onNodeWithTag("event-history-card__chat-button event1").assert(hasText("See Chat"))
+
+        composeRule.onNodeWithTag("event-history-card__chat-button event2").assertExists()
+        composeRule.onNodeWithTag("event-history-card__chat-button event2").assertIsDisplayed()
+        composeRule.onNodeWithTag("event-history-card__chat-button event2").assertHasClickAction()
+        composeRule.onNodeWithTag("event-history-card__chat-button event2").assert(hasText("See Chat"))
+
+        composeRule.onNodeWithTag("event-history-card__chat-button event3").assertExists()
+        composeRule.onNodeWithTag("event-history-card__chat-button event3").assertIsDisplayed()
+        composeRule.onNodeWithTag("event-history-card__chat-button event3").assertHasClickAction()
+        composeRule.onNodeWithTag("event-history-card__chat-button event3").assert(hasText("See Chat"))
+    }
+
+    @Test
+    fun testChatButtonIsNotDisplayed(){
+        composeRule.onNodeWithTag("event-history-card__chat-button NotFinishedEvent").assertDoesNotExist()
+        composeRule.onNodeWithTag("event-history-card__chat-button UserHasNotTakenPartIn").assertDoesNotExist()
+    }
+
+    @Test
+    fun testEventHasName(){
+        composeRule.onNodeWithTag("event-history-card__title event1").assert(hasText("event1"))
+        composeRule.onNodeWithTag("event-history-card__title event2").assert(hasText("event2"))
+        composeRule.onNodeWithTag("event-history-card__title event3").assert(hasText("event3"))
+    }
+
+    @Test
+    fun testEventHasDate(){
+        composeRule.onNodeWithTag("event-history-card__date event1", true).assert(hasText("From 10-10-1997 00:00 to 11-11-1997 00:00"))
+        composeRule.onNodeWithTag("event-history-card__date event2", true).assert(hasText("From 10-10-1998 00:00 to 11-11-1998 00:00"))
+        composeRule.onNodeWithTag("event-history-card__date event3", true).assert(hasText("From 10-10-1999 00:00 to 11-11-1999 00:00"))
+    }
+
+    @Test
+    fun testEventHasDescription(){
+        composeRule.onNodeWithTag("event-history-card__description event1", true).assert(hasText("event1"))
+        composeRule.onNodeWithTag("event-history-card__description event2", true).assert(hasText("event2"))
+        composeRule.onNodeWithTag("event-history-card__description event3", true).assert(hasText("event3"))
     }
 
 }
